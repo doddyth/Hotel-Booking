ae49141e739039e93ab203707fbadaa5c9570d0e
doddyth <doddytrihutomo@gmail.com>
2013-10-28 16:30:30 +0700

    added carousel functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,6 +30,45 @@ angular.module('myApp.controllers', [])
 		}
 	}
   })
+  .directive('imageCarousel', function ($timeout) {
+	return{
+		restrict: 'A',
+		template: '<ul id="foo">' +
+					'<li class="nbs-flexisel-item" ng-repeat="image in hotel.images">' +
+						'<img height="140px" width="140px" src="{{image.uri}}" alt="{{image.uri}}"></img>' +
+					'</li>' +
+				  '</ul>' +
+				  '<div class="clearfix"/>',
+		scope: false,
+		link:function(scope, element){
+			var init = function(){
+				$('#foo').flexisel({
+				visibleItems:5,
+					animationSpeed: 500,
+					autoPlay: false,
+					autoPlaySpeed: 3000,            
+					pauseOnHover: true,
+					enableResponsiveBreakpoints: true,
+					responsiveBreakpoints: { 
+						portrait: { 
+							changePoint:480,
+							visibleItems: 1
+						}, 
+						landscape: { 
+							changePoint:640,
+							visibleItems: 2
+						},
+						tablet: { 
+							changePoint:768,
+							visibleItems: 3
+						}
+					}
+				});
+			};
+			$timeout(init, 0);
+		}
+	}	
+  })
   .controller('NavBarCtrl', ['$scope', '$location', function($scope, $location) { //NAVBAR CONTROLLER
 	$scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
